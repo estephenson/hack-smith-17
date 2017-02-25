8d23dfd3152edef93f07322ee86a38d39d7aed1f
Emma Stephenson <estephenson@smith.edu>
2017-02-25 13:18:18 -0500

    tracking keystrokes

---
+++ file.js
@@ -0,0 +1,17 @@
+//libraries used/things to credit:
+//stackoverflow (lots of javascript)
+
+document.onkeypress = function() {
+   var keyCode = event.which || event.keyCode || 0;
+   console.log(keyCode);
+   if (keyCode == 16) {
+      console.log("yes!")
+   }
+   console.log("good morning!")
+}
+
+//javascript making noise
+//figuring out the uppercase key business and special characters
+//mapping noises to keyboard fully
+
+//chrome extension part
