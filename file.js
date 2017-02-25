0eee2af57ab837d410f1806c8255fe114ddb953b
Emma Stephenson <estephenson@smith.edu>
2017-02-25 15:16:05 -0500

    play a single tone on keypress

--- file.js
+++ file.js
@@ -1,17 +1,64 @@
 //libraries used/things to credit:
 //stackoverflow (lots of javascript)
+//web audio api, several related tutorials
+
+
+//creating an audio context when the page first loads
+var context;
+window.addEventListener('load', init, false);
+function init() {
+  try {
+    // Fix up for prefixing
+    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+    //create a new context
+    context = new AudioContext();
+  }
+  catch(e) {
+    alert("Web Audio API is not supported in this browser. Your password extension will not work.");
+  }
+}
+
+//goal: create a function that runs for the duration of a keypress
+function playTone() {
+   console.log("playTone!");
+   oscillator = context.createOscillator();
+   oscillator.frequency.value = 261.63;
+   oscillator.connect(context.destination);
+   oscillator.start(0);
+   window.setTimeout(stopTone, 100)
+}
+
+//stop playing the tone after a 100ms delay
+function stopTone() {
+   console.log("Stop playing tone");
+   oscillator.stop(0);
+}
+
+// //play the tone while the key is down
+// document.onkeydown = function() {
+//    oscillator = context.createOscillator();
+//    oscillator.frequency.value = 100;
+//    oscillator.connect(context.destination);
+//    oscillator.start(0);
+//    // window.setTimeout(stopTone, 200)
+// }
+//
+// //stop it once the key is up
+// document.onkeyup = function() {
+//    oscillator.stop(0);
+// }
 
 document.onkeypress = function() {
    var keyCode = event.which || event.keyCode || 0;
    console.log(keyCode);
-   if (keyCode == 16) {
-      console.log("yes!")
-   }
+   //uppercase A
+   // if (keyCode == 65) {
+   //    console.log("A key pressed!")
+   //    playTone();
+   // }
+   playTone();
    console.log("good morning!")
 }
 
 //javascript making noise
-//figuring out the uppercase key business and special characters
 //mapping noises to keyboard fully
-
-//chrome extension part
